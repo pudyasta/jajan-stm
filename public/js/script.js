8bed44ce9ca92495330ee3456a290a50f9b8e7c7
hasan <burhannur19@gmail.com>
2020-07-01 23:04:13 +0700

    daftar hampir jadi

--- public/js/script.js
+++ public/js/script.js
@@ -37,33 +37,7 @@ $(document).ready(function(){
     setProgressBar(++current);
     });
     
-    $(".previous").click(function(){
-    
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-    
-    //Remove class active
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-    
-    //show the previous fieldset
-    previous_fs.show();
-    
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-        'display': 'none',
-        'position': 'relative'
-        });
-        previous_fs.css({'opacity': opacity});
-        },
-        duration: 500
-        });
-        setProgressBar(--current);
-    });
+  
     
     function setProgressBar(curStep){
         var percent = parseFloat(100 / steps) * curStep;
--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -9,69 +9,81 @@
     <div class="back-button text-left">
         <a class="btn btn-back display-5 px-3" href="#"><span class="iconify" data-icon="ic:round-arrow-back-ios" data-inline="false"></span> Kembali</a>
     </div>
-    <div class="row justify-content-center ">
-        <div class="col-sm-6 ">
-            <div class="img-dftr">
+    <div class="row justify-content-center">
+        <div class="col-sm-6 gh">
+            <div class="img-dftr justify-content-center">
                 <img src="/img/banner/gambar_lain/undraw_dreamer_gxxi.svg" alt="">
             </div>
         </div>
+        
         <div class="col-sm-6">
         
-            <form id="msform">
+            <form id="msform" class="justify-content-center">
+                <div class="info-proses">
                     <ul id="progressbar">
-                        <li class="active" id="account"></li>
-                        <li id="personal"></li>
-                        <li id="confirm"></li>
+                        <li id="daftar" class="active" ></li>
+                        <li id="konfirmasi"></li>
+                        <li id="selesai"></li>
                     </ul>
+                </div>
                     <fieldset>
                         <div class="form-card">
                             <div class="judul-daftar text-center">
                                 <h1>Selamat Datang!</h1>
                             </div>
                             <div class="judul-item text-center">
-                                <p class="isi-daftar">Wah agar kamu bisa menikmati semua layanan Jajan kamu harus masuk dulu ya!</p>
+                                <h6>Wah agar kamu bisa menikmati semua layanan Jajan kamu harus masuk dulu ya!</h6>
                             </div>
-                                <input type="text" name="nama" placeholder="Nama lengkap" /> 
-                                <input type="text" name="uname" placeholder="Username" /> 
-                                <input type="password" name="pwd" placeholder="Kata sandi" /> 
-                                <input type="password" name="cpwd" placeholder="Konfirmasi kata sandi" />
-                            </div> 
-                            <input type="button" name="next" class="next action-button" value="Next" />
+                            <ul id="progressbar">
+                                <li class="active" id="account"></li>
+                                <li id="personal"></li>
+                                <li id="confirm"></li>
+                            </ul>
+                            <div class="daftar justify-content-center">
+                                <input type="text" name="nama" placeholder="Nama lengkap"/> 
+                                <input type="text" name="uname" placeholder="Username"/> 
+                                <input type="password" name="pwd" placeholder="Kata sandi"/> 
+                                <input type="password" name="cpwd" placeholder="Konfirmasi kata sandi"/>
+                            </div>
+                        </div> 
+                            <input type="button" name="next" class="next action-button" value="Selanjutnya"/>
                     </fieldset>
                     <fieldset>
                         <div class="form-card">
                             <div class="judul-daftar text-center">
                                 <h1>Sedikit lagi..</h1>
                             </div>
-                            <div class="judul-item text-center">
-                                <p class="isi-daftar">Masukan nomor telepon kamu, ini nsntinys akan digunakan untuk menghubungi seller dan banyak fungsi puntar</p>
+                            <div class="judul-item ">
+                                <p class="isi-daftar">Masukan nomor telepon kamu, ini nantinya akan digunakan untuk menghubungi seller dan banyak fungsi pintar</p>
                             </div>
-                                    <input type="text" name="email" placeholder="Nomor Telepon" /> 
-                                    <a href=""><small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small></a>
+                            <div class="daftar">
+                                <input type="text" name="email" placeholder="Nomor Telepon" /> 
+                                <a href=""><small id="emailHelp" class="form-text kirim-kode">Kirim kode verifikasi</small></a>
 
-                                    <small id="emailHelp" class="form-text text-muted">Masukan kode verifikasi yang dikirim ke nomor kamu</small>
-                                    <input type="text" name="uname" placeholder="0-0-0-0" /> 
+                                <small id="emailHelp" class="form-text text-lainnya">Masukan kode verifikasi yang dikirim ke nomor kamu</small>
+                                <input type="text" name="uname" placeholder="0-0-0-0" /> 
 
-                                    <div class="form-check">
-                                        <input class="form-check-input text-left" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                                        <label class="form-check-label" for="exampleRadios2">
-                                        Saya menyetujui segala <a href="">ketentuan dan kebijakan jajan</a>
-                                        </label>
-                                    </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadioInline1">
+                                        <small id="emailHelp" class="form-text radio-input" for="radios">Saya menyetujui segala <a href="">ketentuan dan kebijakan jajan</a></small>
+                                    </label>
+                                </div>
+                            </div>
                         </div> 
-                            <input type="button" name="next" class="next action-button" value="selanjutnya" />
+                            <input type="button" name="next" class="next action-button" value="Selanjutnya" />
                     </fieldset>
                     
                     <fieldset>
                     <div class="form-card">
-                            <div class="judul-daftar text-center">
-                                <h1>Selesai!</h1>
-                            </div>
-                            <div class="judul-item text-center">
-                                <p class="isi-daftar">Selamat @lastgen! <br> Akun kamu berhasil dibuat, yeayy. <br> Selamat menggunakan Jajan!</p>
-                            </div>
-                        </div> 
-                            <button type="summit" class="back-button"> Buka Halaman Beranda</button>
+                        <div class="judul-daftar text-center">
+                            <h1>Selesai!</h1>
+                        </div>
+                        <div class="judul-item text-center">
+                            <p class="isi-daftar">Selamat @lastgen! Akun kamu berhasil dibuat, yeayy. Selamat menggunakan Jajan!</p>
+                        </div>
+                    </div> 
+                            <a href=""><input type="button" name="next" class="next action-button" value="Buka Halaman Beranda" /></a>
                     </fieldset>
                    
                 </form>
